3b310c6f1dfd963e2d472e0d69b07298f59efdd6
CynicalBusiness <matt@vevox.io>
2019-12-10 20:00:15 -0800

    start on two new mods

---
+++ IndustrializationAgriculture/Mod.cs
@@ -0,0 +1,14 @@
+using PipLib.Mod;
+
+namespace MattsMods.Industrialization.Agriculture
+{
+
+    public sealed class Mod : PipMod
+    {
+
+        public override string Name => "Industrialization: Agricultural Automation";
+
+        public override string Prefix => "MMIndy:AA";
+    }
+
+}
