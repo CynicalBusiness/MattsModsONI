41ebfe40bfac5695c093e793c369efd273145b78
CynicalBusiness <matt@vevox.io>
2020-07-12 07:03:17 -0700

    new Adjustable Coolers mod
    Does what it sounds like. Borrows form Adjustable Transformers.

---
+++ AdjustableCoolers/STRINGS/UI.cs
@@ -0,0 +1,16 @@
+using static STRINGS.UI;
+
+namespace MattsMods.AdjustableCoolers.STRINGS
+{
+    public static class UI
+    {
+        public static class UISIDESCREENS
+        {
+            public static class AIRCONDITIONERTEMPERATURESIDESCREEN
+            {
+                public static LocString TITLE = "Cooling Temperature";
+                public static LocString TOOLTIP = $"This device will cool fluids passing through by {FormatAsKeyWord("{0}{1}")}.";
+            }
+        }
+    }
+}
